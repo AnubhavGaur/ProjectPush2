5b41554a8439576eee1820205d5f058fd9be09b7
Anubhav <anubhavgaur1999@gmail.com>
2025-01-16 18:05:01 +0530

    commit

---
+++ src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("testing push");
+        int a = 100;
+        System.out.println("value of a is "+ a);
+    }
+}
